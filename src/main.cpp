8a6e4a91fa00f2c86abeb8834e27941927006d41
vladislav <vladislavrykov@hotmail.com>
2020-01-07 09:40:16 +0100

    sensor struct mod

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@
 #define MKR_TX_PIN                          26
 #define MKR_RX_PIN                          27
 
-#define DATA_SEND_PERIOD                    30
+#define DATA_SEND_PERIOD                    60000
 
 HardwareSerial bus_wis(2);
 HardwareSerial bus_mkr(1);
@@ -54,10 +54,6 @@ void bus_task_mkr (void *parameter);
 void read_sensors_data(sensors_data_t *sensors_data);
 void sd_add_record(const sensors_data_t *sensors_data);
 
-uint8_t bus_protocol_sensor_data_payload_decode(
-    sensors_data_t *sensor_data,
-    const uint8_t *payload,
-    const uint8_t payload_length);
 uint8_t bus_protocol_serial_receive(Stream *serial, uint8_t *data, uint8_t *data_length);
 
 void sleep_mcu(const uint32_t ms);
@@ -144,69 +140,47 @@ void loop() {
 
 void sensors_task (void *parameter) {
     sensors_data_t sensors_data;
+    sensors_data.board_id = BUS_PROTOCOL_BOARD_ID_ESP;
     
-    while (1) {
+    for(;;) {
         // read data
-        // read_sensors_data(&sensors_data);
+        read_sensors_data(&sensors_data);
 
         // write new sd record
-        // if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdTRUE) {
-        //     sd_add_record(&sensors_data);
+        if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdTRUE) {
+            sd_add_record(&sensors_data);
 
-        //    xSemaphoreGive(xBinarySemaphore);
-        // }
+           xSemaphoreGive(xBinarySemaphore);
+        }
         
-        sleep_mcu(DATA_SEND_PERIOD);
+        vTaskDelay((DATA_SEND_PERIOD + random(5000)) / portTICK_PERIOD_MS);
     }
 }
  
 void bus_task_wis (void *parameter) {
     sensors_data_t sensors_data;
-    board_id_t talking_with = BUS_PROTOCOL_BOARD_ID_UNKNOWN;
 
     uint8_t buffer[BUS_PROTOCOL_MAX_DATA_SIZE] = {0};
     uint8_t buffer_length = 0;
 
-    while (1) {
+    for(;;) {
         if (bus_protocol_serial_receive(&bus_wis, buffer, &buffer_length)) {
-            // Serial.print(F("Received message: "));
-            // for (uint8_t i = 0; i < buffer_length; i++) {
-            //     Serial.printf("%02X", buffer[i]);
-            // }
-            // Serial.println();
-
             switch (bus_protocol_packet_decode(buffer, buffer_length, buffer, &buffer_length)) {
-                case BUS_PROTOCOL_PACKET_TYPE_TRANSMIT_REQUEST :
-                    Serial.println(F("WIS TRANSMIT REQUEST"));
-                    talking_with = bus_protocol_transmit_request_decode(buffer, buffer_length);
-                    
-                    bus_protocol_transmit_grant_encode(talking_with, buffer, &buffer_length);
-                    bus_wis.write(buffer, buffer_length);
-                    
-                    break;
                 case BUS_PROTOCOL_PACKET_TYPE_DATA_SEND :
-                    Serial.println(F("WIS DATA SEND"));
-                    Serial.printf("%d bytes\r\n", buffer_length);
+                    ESP_LOGD(TAG, "WIS DATA SEND");
+                    ESP_LOGD(TAG, "%d bytes", buffer_length);
                     
                     memcpy(&sensors_data, buffer, sizeof(sensors_data));
-                    // if (bus_protocol_sensor_data_payload_decode(
-                    //                                     &sensors_data,
-                    //                                     buffer, 
-                    //                                     buffer_length)) 
-                    // {
-                        // ACK data send
-                        bus_protocol_packet_encode(BUS_PROTOCOL_PACKET_TYPE_ACK, buffer, 0, buffer, &buffer_length);
-                        bus_wis.write(buffer, buffer_length);
 
-                        // write new sd record 
-                        if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdTRUE) {
-                            sd_add_record(&sensors_data);
+                    bus_protocol_packet_encode(BUS_PROTOCOL_PACKET_TYPE_ACK, buffer, 0, buffer, &buffer_length);
+                    bus_wis.write(buffer, buffer_length);
 
-                            xSemaphoreGive(xBinarySemaphore);
-                        }
-                    // }
+                    // write new sd record 
+                    if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdTRUE) {
+                        sd_add_record(&sensors_data);
 
-                    talking_with = BUS_PROTOCOL_BOARD_ID_UNKNOWN;
+                        xSemaphoreGive(xBinarySemaphore);
+                    }
                 default:
                     break;
             }
@@ -217,52 +191,30 @@ void bus_task_wis (void *parameter) {
 
 void bus_task_mkr (void *parameter) {
     sensors_data_t sensors_data;
-    board_id_t talking_with = BUS_PROTOCOL_BOARD_ID_UNKNOWN;
 
     uint8_t buffer[BUS_PROTOCOL_MAX_DATA_SIZE] = {0};
     uint8_t buffer_length = 0;
 
-    while (1) {
+    for(;;) {
         if (bus_protocol_serial_receive(&bus_mkr, buffer, &buffer_length)) {
-            // Serial.print(F("MKR Received message: "));
-            // for (uint8_t i = 0; i < buffer_length; i++) {
-            //     Serial.printf("%02X", buffer[i]);
-            // }
-            // Serial.println();
-
             switch (bus_protocol_packet_decode(buffer, buffer_length, buffer, &buffer_length)) {
-                case BUS_PROTOCOL_PACKET_TYPE_TRANSMIT_REQUEST :
-                    Serial.println(F("MKR TRANSMIT REQUEST"));
-                    talking_with = bus_protocol_transmit_request_decode(buffer, buffer_length);
-                    
-                    bus_protocol_transmit_grant_encode(talking_with, buffer, &buffer_length);
-                    bus_wis.write(buffer, buffer_length);
-                    
-                    break;
                 case BUS_PROTOCOL_PACKET_TYPE_DATA_SEND :
-                    Serial.println(F("MKR DATA SEND"));
-                    Serial.printf("%d bytes\r\n", buffer_length);
+                    ESP_LOGD(TAG, "MKR DATA SEND");
+                    ESP_LOGD(TAG, "%d bytes\r\n", buffer_length);
 
                     
                     memcpy(&sensors_data, buffer, sizeof(sensors_data));
-                    // if (bus_protocol_sensor_data_payload_decode(
-                    //                                     &sensors_data,
-                    //                                     buffer, 
-                    //                                     buffer_length)) 
-                    // {
-                        // ACK data send
-                        bus_protocol_packet_encode(BUS_PROTOCOL_PACKET_TYPE_ACK, buffer, 0, buffer, &buffer_length);
-                        bus_mkr.write(buffer, buffer_length);
 
-                        // write new sd record 
-                        if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdTRUE) {
-                            sd_add_record(&sensors_data);
+                    // ACK data send
+                    bus_protocol_packet_encode(BUS_PROTOCOL_PACKET_TYPE_ACK, buffer, 0, buffer, &buffer_length);
+                    bus_mkr.write(buffer, buffer_length);
 
-                            xSemaphoreGive(xBinarySemaphore);
-                        }
-                    // }
+                    // write new sd record 
+                    if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdTRUE) {
+                        sd_add_record(&sensors_data);
 
-                    talking_with = BUS_PROTOCOL_BOARD_ID_UNKNOWN;
+                        xSemaphoreGive(xBinarySemaphore);
+                    }
                 default:
                     break;
             }
@@ -285,18 +237,18 @@ void read_sensors_data(sensors_data_t *sensors_data) {
 
     // Check if any reads failed and exit early (to try again).
     if (isnan(hum) || isnan(temp)) {
-      Serial.println(F("Failed to read from DHT sensor!"));
+      ESP_LOGE(TAG, "Failed to read from DHT sensor!");
     }
 
-    // Serial.println(F("Sensors data:"));
-    // Serial.println(sensors_data->soil_moisture_0);
-    // Serial.println(sensors_data->soil_moisture_1);
-    // Serial.println(sensors_data->dht_temp);
-    // Serial.println(sensors_data->dht_hum);
+    ESP_LOGD(TAG, "Sensors data:");
+    ESP_LOGD(TAG, sensors_data->soil_moisture_0);
+    ESP_LOGD(TAG, sensors_data->soil_moisture_1);
+    ESP_LOGD(TAG, sensors_data->dht_temp);
+    ESP_LOGD(TAG, sensors_data->dht_hum);
 }
 
 void sd_add_record(const sensors_data_t *sensors_data) {
-    Serial.printf("writen sd: board_id = %d, time: %s, sm_0 = %u, sm_1 = %u, sm_2 = %u, t = %0.2f, h = %0.2f\r\n",
+    ESP_LOGD(TAG, "writen sd: board_id = %d, time: %s, sm_0 = %u, sm_1 = %u, sm_2 = %u, t = %0.2f, h = %0.2f\r\n",
                     sensors_data->board_id, timeClient.getFormattedTime(), sensors_data->soil_moisture_0, 
                     sensors_data->soil_moisture_1, sensors_data->soil_moisture_2,
                     sensors_data->dht_temp, sensors_data->dht_hum);
@@ -311,39 +263,6 @@ void sd_add_record(const sensors_data_t *sensors_data) {
 
         Serial.println(F("SD written"));
     }
-}
-
-uint8_t bus_protocol_sensor_data_payload_decode(
-    sensors_data_t *sensor_data,
-    const uint8_t *payload,
-    const uint8_t payload_length)
-{
-    uint8_t p_len = 0;
-
-    memcpy(&sensor_data->board_id, &payload[p_len], sizeof(sensor_data->board_id));
-    p_len++;
-
-    memcpy(&sensor_data->utc, &payload[p_len], sizeof(sensor_data->utc));
-    p_len += sizeof(sensor_data->utc);
-
-    memcpy(&sensor_data->soil_moisture_0, &payload[p_len], sizeof(sensor_data->soil_moisture_0));
-    p_len += sizeof(sensor_data->soil_moisture_0);
-
-    memcpy(&sensor_data->soil_moisture_1, &payload[p_len], sizeof(sensor_data->soil_moisture_1));
-    p_len += sizeof(sensor_data->soil_moisture_1);
-
-    if (sensor_data->board_id == BUS_PROTOCOL_BOARD_ID_MKR) {
-        memcpy(&sensor_data->soil_moisture_2, &payload[p_len], sizeof(sensor_data->soil_moisture_2));
-        p_len += sizeof(sensor_data->soil_moisture_2);
-    }
-
-    memcpy(&sensor_data->dht_temp, &payload[p_len], sizeof(sensor_data->dht_temp));
-    p_len += sizeof(sensor_data->dht_temp);
-
-    memcpy(&sensor_data->dht_hum, &payload[p_len], sizeof(sensor_data->dht_hum));
-    p_len += sizeof(sensor_data->dht_hum);
-
-    return p_len == payload_length;
 }
 
 uint8_t bus_protocol_serial_receive(Stream *serial, uint8_t *data, uint8_t *data_length) {
